e1a547df0737531e62d34bc6634256c856f4e5e1
Bryce David Landry <brycedavid66@gmail.com>
2021-08-18 16:32:25 -0500

    first commit

---
+++ app/script.js
@@ -0,0 +1,34 @@
+const navbarSearchLink = document.getElementById("search-link");
+const navbarSearchElement = document.getElementById("navbar-search-container");
+
+function setSearchInvisible() {
+  navbarSearchElement.style.visibility = "hidden";
+}
+
+function setSearchVisible() {
+  navbarSearchElement.style.visibility = "visible";
+}
+
+function setSearchActive() {
+  navbarSearchLink.classList.add("active");
+}
+
+function setSearchInactive() {
+  navbarSearchLink.classList.remove("active");
+}
+
+function getParameterByName(name, url = window.location.href) {
+  name = name.replace(/[\[\]]/g, "\\$&");
+  var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+    results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return "";
+  return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+var param = getParameterByName("search");
+
+if (document.querySelector("#search-result")) {
+  const result = document.querySelector("#search-result");
+  result.innerHTML = param;
+}
