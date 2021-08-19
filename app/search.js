86617c7d7a2ee2b344b03e920a9a079b0fa44589
Bryce David Landry <brycedavid66@gmail.com>
2021-08-19 17:18:37 -0500

    Displays name/image of cats/dogs on search page

--- app/search.js
+++ app/search.js
@@ -3,17 +3,6 @@ var pf = new petfinder.Client({
   secret: "WhuKLwumWocRsjzuQYPVSC6ZybxuMdhVCRXYIIW6",
 });
 
-// pf.animal
-//   .search({
-//     type: "Dog",
-//     breed: "Bernedoodle",
-//     page: 1,
-//     limit: 100,
-//   })
-//   .then((resp) => {
-//     console.log(resp);
-//   });
-
 function getParameterByName(name, url = window.location.href) {
   name = name.replace(/[\[\]]/g, "\\$&");
   var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
@@ -23,7 +12,6 @@ function getParameterByName(name, url = window.location.href) {
   return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
 
-// let query = getParameterByName("search");
 let animal = getParameterByName("animal");
 
 if (document.querySelector("#search-result")) {
@@ -40,7 +28,28 @@ pf.animal
   })
   .then((resp) => {
     if (document.querySelector("#results-display")) {
-      const resultDisplay = document.querySelector("results-display");
       console.log(resp.data.animals);
+      displayResults(resp.data.animals);
     }
   });
+
+function displayResults(data) {
+  const resultDisplay = document.querySelector("#results-display");
+  for (let i of data) {
+    let element = document.createElement("p");
+    let node = document.createTextNode(i.name);
+    element.appendChild(node);
+    resultDisplay.appendChild(element);
+
+    if (i.photos.length != 0) {
+      element = document.createElement("img");
+      element.setAttribute("src", i.photos[0].small);
+      resultDisplay.appendChild(element);
+    } else {
+      element = document.createElement("p");
+      node = document.createTextNode("No photo available");
+      element.appendChild(node);
+      resultDisplay.appendChild(element);
+    }
+  }
+}
