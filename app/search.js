60a8f82cee90ee33f6643101b5d91087aafec4e0
Bryce David Landry <brycedavid66@gmail.com>
2021-08-19 13:21:35 -0500

    added select element in navbar search, re-organized topnav, attempt at making request from user input

--- app/search.js
+++ app/search.js
@@ -1,3 +1,19 @@
+var pf = new petfinder.Client({
+  apiKey: "YeI5i5zLHnqvUoBxfJcjseCpBDQcZSS6ecZKJouXs07aejuKfK",
+  secret: "WhuKLwumWocRsjzuQYPVSC6ZybxuMdhVCRXYIIW6",
+});
+
+// pf.animal
+//   .search({
+//     type: "Dog",
+//     breed: "Bernedoodle",
+//     page: 1,
+//     limit: 100,
+//   })
+//   .then((resp) => {
+//     console.log(resp);
+//   });
+
 function getParameterByName(name, url = window.location.href) {
   name = name.replace(/[\[\]]/g, "\\$&");
   var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
@@ -7,9 +23,24 @@ function getParameterByName(name, url = window.location.href) {
   return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
 
-var param = getParameterByName("search");
+var query = getParameterByName("search");
+var animal = getParameterByName("animal");
 
 if (document.querySelector("#search-result")) {
   const result = document.querySelector("#search-result");
-  result.innerHTML = param;
+  result.innerHTML = query + " " + animal;
 }
+
+pf.animal
+  .search({
+    type: animal,
+    breed: "all",
+    page: 1,
+    limit: 100,
+  })
+  .then((resp) => {
+    if (document.querySelector("#results-display")) {
+      const resultDisplay = document.querySelector("results-display");
+      resultDisplay.innerHTML = resp;
+    }
+  });
