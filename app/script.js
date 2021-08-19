54da7a687f8682430a73874c1efa2c8a47de30ef
Bryce David Landry <brycedavid66@gmail.com>
2021-08-19 09:36:16 -0500

    attempts at using petfinder sdk and API

--- app/script.js
+++ app/script.js
@@ -1,3 +1,20 @@
+let petfinder = require("@petfinder/petfinder-js");
+let client = new petfinder.Client({
+  apiKey: "YeI5i5zLHnqvUoBxfJcjseCpBDQcZSS6ecZKJouXs07aejuKfK",
+  secret: "WhuKLwumWocRsjzuQYPVSC6ZybxuMdhVCRXYIIW6",
+});
+
+client.animal
+  .search({
+    type: "Dog",
+    breed: "Bernedoodle",
+    page: 1,
+    limit: 100,
+  })
+  .then((resp) => {
+    console.log(resp);
+  });
+
 const navbarSearchLink = document.getElementById("search-link");
 const navbarSearchElement = document.getElementById("navbar-search-container");
 
@@ -16,27 +33,3 @@ function setSearchActive() {
 function setSearchInactive() {
   navbarSearchLink.classList.remove("active");
 }
-
-function getParameterByName(name, url = window.location.href) {
-  name = name.replace(/[\[\]]/g, "\\$&");
-  var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-    results = regex.exec(url);
-  if (!results) return null;
-  if (!results[2]) return "";
-  return decodeURIComponent(results[2].replace(/\+/g, " "));
-}
-
-var param = getParameterByName("search");
-
-if (document.querySelector("#search-result")) {
-  const result = document.querySelector("#search-result");
-  result.innerHTML = param;
-}
-
-var petfinder = require("@petfinder/petfinder-js");
-var client = new petfinder.Client({
-  apiKey: "YeI5i5zLHnqvUoBxfJcjseCpBDQcZSS6ecZKJouXs07aejuKfK",
-  secret: "WhuKLwumWocRsjzuQYPVSC6ZybxuMdhVCRXYIIW6",
-});
-
-client.animal.search();
