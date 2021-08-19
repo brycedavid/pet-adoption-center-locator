6414b599ed3561b1e76c4d3e7742612703a16245
Bryce David Landry <brycedavid66@gmail.com>
2021-08-19 10:11:48 -0500

    another attempt at using petfinder SDK

--- app/auth.js
+++ app/auth.js
@@ -1,5 +1,10 @@
-import { Client } from "../node_modules/petfinder-js";
-let client = new Client({
+// import { Client } from "../node_modules/petfinder-js";
+// let client = new Client({
+//   apiKey: "YeI5i5zLHnqvUoBxfJcjseCpBDQcZSS6ecZKJouXs07aejuKfK",
+//   secret: "WhuKLwumWocRsjzuQYPVSC6ZybxuMdhVCRXYIIW6",
+// });
+
+var pf = new petfinder.Client({
   apiKey: "YeI5i5zLHnqvUoBxfJcjseCpBDQcZSS6ecZKJouXs07aejuKfK",
   secret: "WhuKLwumWocRsjzuQYPVSC6ZybxuMdhVCRXYIIW6",
 });
